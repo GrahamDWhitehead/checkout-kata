0533160a6c56ece27bb3a3fed4e8cd51cde66f0b
Graham Whitehead <gwhitehead@ad.ceguk.net>
2020-08-09 14:03:58 +0100

    - Added first unit test for adding items to basket

---
+++ CheckoutKata/CheckoutKata.UnitTests/Areas/Basket/Fascades/BasketTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CheckoutKata.UnitTests.Areas.Basket.Fascades
+{
+    public class BasketTests
+    {
+        #region Fields
+
+        private IBasket _sut;
+
+        #endregion
+
+        #region Constructor
+
+        public BasketTests()
+        {
+            _sut = new Basket();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Theory]
+        [InlineData('A')]
+        [InlineData('B')]
+        [InlineData('C')]
+        [InlineData('D')]
+        public void AddItem_ValidSkyProvided_ReturnsTrue(char sku)
+        {
+            // Act
+            var result = _sut.AddItem(sku);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        #endregion
+    }
+}
